1c7828cc9568d01512f48928310cef3bcb6a9e60
yan zhang <circusburger63@yahoo.com>
2022-05-07 14:23:51 -0700

    alert box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import './App.css';
-import Files from './components/Files';
+import Files from './components/Files/Files';
 import filesData from "./files.json";
 
 function App() {  
